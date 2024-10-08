8f0e8cd1341513d4a61ec9886bacfb26c7e15060
Daniel Filip <filipdaniel2000@yahoo.com>
2024-10-08 22:13:37 +0300

    palindrom

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,25 +1,13 @@
-public class Factorial {
+public class Ex1 {
         public static void main(String args[]) {
             String number = System.console().readLine();
             int n = Integer.parseInt(number);
-            if ( n <= 12 && n >= 0) {
+            if (n <= 12 && n >= 0) {
                 int fact = 1;
-                for(int i = 1; i <= n; ++i ) {
+                for (int i = 1; i <= n; ++i) {
                     fact *= i;
                 }
                 System.out.println(fact);
             }
         }
-//        public static void main(String args[]) {
-//            String number = System.console().readLine();
-//            int n = Integer.parseInt(number);
-//            int c = 0;
-//            int f = n;
-//            while (n != 0) {
-//                c = c * 10 + n % 10;
-//                n /= 10;
-//            }
-//            if(f == c) System.out.println("Palindrom");
-//            else System.out.println("Not Palindrom");
-//        }
 }
