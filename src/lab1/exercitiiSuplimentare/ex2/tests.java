40e36999e5534cf06d2caf24da9dc6d2192c6881
Daniel Filip <filipdaniel2000@yahoo.com>
2024-10-10 23:09:29 +0300

    exsup

--- src/lab1/exercitiiSuplimentare/ex2/tests.java
+++ src/lab1/exercitiiSuplimentare/ex2/tests.java
@@ -7,23 +7,23 @@ public class tests {
 
     @Test
     public void testPositivePalindrome() {
-        assertTrue(Palindrom.isPalindrome(121)); // Palindrom
-        assertTrue(Palindrom.isPalindrome(12321)); // Palindrom
-        assertTrue(Palindrom.isPalindrome(1)); // Palindrom (numerele cu o singura cifra)
-        assertTrue(Palindrom.isPalindrome(0)); // Palindrom (cazul 0)
+        assertTrue(Palindrom.isPalindrome(121));    // Palindrom
+        assertTrue(Palindrom.isPalindrome(12321));  // Palindrom
+        assertTrue(Palindrom.isPalindrome(1));      // Palindrom (numerele cu o singura cifra)
+        assertTrue(Palindrom.isPalindrome(0));      // Palindrom (cazul 0)
     }
 
     @Test
     public void testNegativePalindrome() {
-        assertTrue(Palindrom.isPalindrome(-121)); // Palindrom (-121 citit invers e tot -121)
+        assertTrue(Palindrom.isPalindrome(-121));   // Palindrom (-121 citit invers e tot -121)
         assertTrue(Palindrom.isPalindrome(-12321)); // Palindrom (-12321)
     }
 
     @Test
     public void testNotPalindrome() {
-        assertFalse(Palindrom.isPalindrome(123)); // Nu este palindrom
-        assertFalse(Palindrom.isPalindrome(100)); // Nu este palindrom
-        assertFalse(Palindrom.isPalindrome(10));  // Nu este palindrom
+        assertFalse(Palindrom.isPalindrome(123));   // Nu este palindrom
+        assertFalse(Palindrom.isPalindrome(100));   // Nu este palindrom
+        assertFalse(Palindrom.isPalindrome(10));    // Nu este palindrom
     }
 }
 
