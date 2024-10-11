f08cacfb0c6317680f89783e38fbda59c6114b12
Daniel Filip <filipdaniel2000@yahoo.com>
2024-10-11 21:23:56 +0300

    exsup

---
+++ src/lab1/exercitiiSuplimentare/ex4/Tablouri.java
@@ -0,0 +1,66 @@
+package lab1.exercitiiSuplimentare.ex4;
+
+public class Tablouri {
+
+    // Functia pentru a construi si afisa matricea m[i][j] = a[i] * b[j]
+    public static double[][] calculeazaMatriceaM(double[] a, double[] b) {
+        int n = a.length;
+        int m = b.length;
+        double[][] matriceM = new double[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matriceM[i][j] = a[i] * b[j];
+            }
+        }
+
+        return matriceM;
+    }
+
+    // Functia pentru a construi si afisa vectorul v[i] = min{a[i], b[i]}
+    public static double[] calculeazaVectorulV(double[] a, double[] b) {
+        int n = a.length;
+        double[] vectorV = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            vectorV[i] = Math.min(a[i], b[i]);
+        }
+
+        return vectorV;
+    }
+
+    // Functia pentru afisarea unei matrice
+    public static void afiseazaMatrice(double[][] matrice) {
+        for (double[] linie : matrice) {
+            for (double element : linie) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Functia pentru afișarea unui vector
+    public static void afiseazaVector(double[] vector) {
+        for (double element : vector) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Initializam doua tablouri constante de numere reale
+        double[] a = {1.5, 2.5, 3.5};
+        double[] b = {4.0, 5.0, 6.0};
+
+        // Calculam și afisam matricea m
+        double[][] matriceM = calculeazaMatriceaM(a, b);
+        System.out.println("Matricea M:");
+        afiseazaMatrice(matriceM);
+
+        // Calculam si afisam vectorul v
+        double[] vectorV = calculeazaVectorulV(a, b);
+        System.out.println("Vectorul V:");
+        afiseazaVector(vectorV);
+    }
+}
+
---
+++ src/lab1/exercitiiSuplimentare/ex4/tests.java
@@ -0,0 +1,56 @@
+package lab1.exercitiiSuplimentare.ex4;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class tests {
+
+    @Test
+    public void testCalculeazaMatriceaM() {
+        double[] a = {1.5, 2.5, 3.5};
+        double[] b = {4.0, 5.0, 6.0};
+        double[][] expectedM = {
+                {6.0, 7.5, 9.0},
+                {10.0, 12.5, 15.0},
+                {14.0, 17.5, 21.0}
+        };
+
+        // Verificam dacă matricea calculata este corecta
+        assertArrayEquals(expectedM, Tablouri.calculeazaMatriceaM(a, b));
+    }
+
+    @Test
+    public void testCalculeazaVectorulV() {
+        double[] a = {1.5, 2.5, 3.5};
+        double[] b = {4.0, 5.0, 2.0};
+        double[] expectedV = {1.5, 2.5, 2.0};
+
+        // Verificam dacă vectorul calculat este corect
+        assertArrayEquals(expectedV, Tablouri.calculeazaVectorulV(a, b), 0.0001);
+    }
+
+    @Test
+    public void testMatriceaMForNegativeValues() {
+        double[] a = {-1.5, -2.5, -3.5};
+        double[] b = {4.0, -5.0, 6.0};
+        double[][] expectedM = {
+                {-6.0, 7.5, -9.0},
+                {-10.0, 12.5, -15.0},
+                {-14.0, 17.5, -21.0}
+        };
+
+        // Verificam daca matricea calculata este corecta pentru valori negative
+        assertArrayEquals(expectedM, Tablouri.calculeazaMatriceaM(a, b));
+    }
+
+    @Test
+    public void testVectorulVForNegativeValues() {
+        double[] a = {-1.5, 2.5, -3.5};
+        double[] b = {4.0, -5.0, -2.0};
+        double[] expectedV = {-1.5, -5.0, -3.5};
+
+        // Verificam daca vectorul calculat este corect pentru valori negative
+        assertArrayEquals(expectedV, Tablouri.calculeazaVectorulV(a, b), 0.0001);
+    }
+}
+
