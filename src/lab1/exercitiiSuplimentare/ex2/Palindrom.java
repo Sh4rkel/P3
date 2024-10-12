3bcb6451310941f4b10f4e491ce8b73ff4d545ba
Daniel Filip <filipdaniel2000@yahoo.com>
2024-10-12 16:38:00 +0300

    exsup

--- src/lab1/exercitiiSuplimentare/ex2/Palindrom.java
+++ src/lab1/exercitiiSuplimentare/ex2/Palindrom.java
@@ -6,7 +6,7 @@ package lab1.exercitiiSuplimentare.ex2;
  * @author Daniel Filip
  */
 
-
+// Urmatoarea versiune este actualizata, am lasat la final si prima versiune
 
 public class Palindrom {
 
@@ -33,3 +33,19 @@ public class Palindrom {
         return true; // Daca nu am gasit diferente, este palindrom
     }
 }
+
+/**
+ *  !!! Aceasta varianta nu a fost buna, deaorece nu putem stii cate cifre are numarul introdus,
+ *  dar mai este si o problema de spatiu si timp in rularea acestui algoritm destul de basic pentru palindrom !!!
+ * @param numar
+ *
+ * public static boolean isPalindrom(int numar) {
+ *     int copieNumarIntial = numar;
+ *     int invers = 0;
+ *     while (copieNumarInitial > 0) {
+ *         invers *= 10 + copieNumarInital % 10;
+ *         copieNumarInitial /= 10;
+ *     }
+ *     return (numar == invers)
+ * }
+ */
